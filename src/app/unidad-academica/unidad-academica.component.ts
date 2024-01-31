55c3a18d4abd05c599679fe33556e59dd9353400
danielniebla <82241926+danielniebla@users.noreply.github.com>
2024-01-31 12:28:48 -0700

    aaa

--- src/app/unidad-academica/unidad-academica.component.ts
+++ src/app/unidad-academica/unidad-academica.component.ts
@@ -95,7 +95,7 @@ export class UnidadAcademicaComponent implements OnInit{
   cambiarCarrera(id: number){
     // localStorage.setItem('idCarrera', id.toString());
     window.location.href = 'https://yobani.onrender.com/';
-    window.location.reload();
+    // window.location.reload();
   }
   dropDown() {
     this.open = !this.open;
